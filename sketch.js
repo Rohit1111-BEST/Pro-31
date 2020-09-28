1a7527338d2419d543a2bc6dcd9dcf80de32bb4d
Rohit1111-BEST <65074708+Rohit1111-BEST@users.noreply.github.com>
2020-09-28 13:55:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var particals = []
 var plinko = []
 var divisions = []
+var divisionHeight=300;
 
 var engine, world;
 
@@ -16,40 +17,16 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  for(var k =0; k <= width; k = k+ 80){
-    divisions.push(new Division(k, height-divisionHeight/2,10,divisionHeight));
-  }
-    for (var j =40; j <=width; j=j+50)
-{
-  plinko.push(new Plinko(j,75));
+  
 }
-for (var j = 15; j <=width-10; j=j+50)
-{
-plinko.push(new Plinko(j,175));
-
-}
-
-for (var j =0; j < divisions.length; j++){
-
-  divisions[j].display();
-}
-for (var j =0; j < plinko.length; j++){
-
-  plinko[j].display();
-}
-}
-
-
-
-var divisionHeight=300;
-
-
 
 
 
 
 function draw() {
 background(255,255,255);  
+Engine.update(engine)
+
 
 if(frameCount%60===0){
   particals.push(new Partical(random(width/2-10, width/2+10),10,10));
@@ -63,6 +40,26 @@ for (var j =0; j < particals.length; j++){
 for (var j =0; j < particals.length; j++)
 { particals[j].display(); }
 
+for(var k =0; k <= width; k = k+ 80){
+  divisions.push(new Division(k, height-divisionHeight/2,10,divisionHeight));
+}
+  for (var j =40; j <=width; j=j+50)
+{
+plinko.push(new Plinko(j,75));
+}
+for (var j = 15; j <=width-10; j=j+50)
+{
+plinko.push(new Plinko(j,175));
 
 }
 
+for (var j =0; j < divisions.length; j++){
+
+divisions[j].display();
+}
+for (var j =0; j < plinko.length; j++){
+
+plinko[j].display();
+}
+
+}
